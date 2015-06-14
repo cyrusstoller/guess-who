a2181874cb0881703fca5212455195cc0b6c558d
Cyrus Stoller <cyrus.stoller@gmail.com>
2015-06-14 13:36:34 -0700

    Typo in the symlink

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,7 +28,7 @@ set :linked_files, fetch(:linked_files, []).push('.env', 'data/people.sqlite3.db
 
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/sockets', 'public/img/thumbnails')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/sockets', 'public/img/thumbnail')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
